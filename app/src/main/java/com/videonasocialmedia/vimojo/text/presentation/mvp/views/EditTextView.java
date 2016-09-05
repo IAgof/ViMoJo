074bbdea47b0c52752d29f33b3a1e70a2ca08ae6
ruth <r.toro@videona.com>
2016-09-05 18:11:51 +0200

    -Added new VideoEditTextActivity, Layout, View, Presenter -Add new imageButton in EditActivity to navigate to the VideoEditTextActivity -Add new custom to set maximum characters per line fot edit Text

---
+++ app/src/main/java/com/videonasocialmedia/vimojo/text/presentation/mvp/views/EditTextView.java
@@ -0,0 +1,29 @@
+package com.videonasocialmedia.vimojo.text.presentation.mvp.views;
+
+import android.graphics.drawable.Drawable;
+
+import com.videonasocialmedia.vimojo.model.entities.editor.media.Video;
+
+import java.util.List;
+
+/**
+ * Created by ruth on 1/09/16.
+ */
+public interface EditTextView {
+
+    //void showTrimBar(int videoStartTime, int videoStopTime, int videoFileDuration);
+
+    void playPreview();
+
+    void pausePreview();
+
+    void seekTo(int timeInMsec);
+
+    void showPreview(List<Video> movieList);
+
+    void showError(String message);
+
+    void showText(Drawable drawable);
+
+
+}
