5b764ea76e93809d2fcf95d3f8445764af2747ee
alvaro martinez <a.martinez@videona.com>
2016-09-08 16:03:10 +0200

    - Manage add text to video if video has been trimmed

--- app/src/main/java/com/videonasocialmedia/vimojo/text/domain/ModifyVideoTextAndPositionUseCase.java
+++ app/src/main/java/com/videonasocialmedia/vimojo/text/domain/ModifyVideoTextAndPositionUseCase.java
@@ -1,62 +1,70 @@
 package com.videonasocialmedia.vimojo.text.domain;
 
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
 
 import com.videonasocialmedia.transcoder.MediaTranscoder;
 import com.videonasocialmedia.transcoder.MediaTranscoderListener;
 import com.videonasocialmedia.transcoder.format.VideonaFormat;
 import com.videonasocialmedia.transcoder.overlay.Image;
-import com.videonasocialmedia.transcoder.overlay.Overlay;
-import com.videonasocialmedia.vimojo.model.entities.editor.media.Media;
 import com.videonasocialmedia.vimojo.model.entities.editor.media.Video;
 import com.videonasocialmedia.vimojo.text.util.TextToDrawable;
 import com.videonasocialmedia.vimojo.utils.Constants;
 
 import java.io.IOException;
-import java.util.LinkedList;
 
 /**
  * Created by alvaro on 1/09/16.
  */
 public class ModifyVideoTextAndPositionUseCase {
 
-    public void addTextToVideo (Video videoToEdit, VideonaFormat format, String text, String textPosition, MediaTranscoderListener listener) {
+    public void addTextToVideo (Video videoToEdit, VideonaFormat format, String text, String textPosition,
+                                MediaTranscoderListener listener) {
         try {
-
-            Drawable textDrawable = TextToDrawable.createDrawableWithTextAndPosition(text, textPosition);
-
-            Image imageText = new Image(textDrawable, Constants.DEFAULT_VIMOJO_WIDTH,Constants.DEFAULT_VIMOJO_HEIGHT);
 
             videoToEdit.setTempPathFinished(false);
             videoToEdit.setTempPath();
+            //videoToEdit.setText(text, textPosition);
 
             if(videoToEdit.isEdited()) {
-
-                MediaTranscoder.getInstance().transcodeTrimAndOverlayImageToVideo(videoToEdit.getMediaPath(),
-                        videoToEdit.getTempPath(),format, listener, imageText, videoToEdit.getStartTime(),
-                        videoToEdit.getStopTime());
+                transcodeTrimAndOverlayImageToVideo(videoToEdit.getMediaPath(), videoToEdit.getTempPath(),
+                        format, listener, text, textPosition, videoToEdit.getStartTime(), videoToEdit.getStartTime());
             } else {
-
-                MediaTranscoder.getInstance().transcodeAndOverlayImageToVideo(videoToEdit.getMediaPath(), videoToEdit.getTempPath(),
-                        format, listener, imageText);
+                transcodeTrimAndOverlayImageToVideo(videoToEdit.getMediaPath(), videoToEdit.getTempPath(),
+                        format, listener, text, textPosition);
             }
         } catch (IOException e) {
             // TODO(javi.cabanas): 2/8/16 mangage io expception on external library and send onTranscodeFailed if neccessary
         }
     }
 
-    public void waitForOutputFilesFinished(Video videoToEdit) {
+    private void transcodeTrimAndOverlayImageToVideo(String mediaPath, String tempPath, VideonaFormat format,
+                                                     MediaTranscoderListener listener, String text,
+                                                     String textPosition) throws IOException {
 
-            if (videoToEdit.isEdited()) {
-                while (!videoToEdit.outputVideoIsFinished()) {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
+        Image imageText = getImageFromTextAndPosition(text, textPosition);
+
+        MediaTranscoder.getInstance().transcodeAndOverlayImageToVideo(mediaPath,
+                tempPath,format, listener, imageText);
     }
+
+    private void transcodeTrimAndOverlayImageToVideo(String mediaPath, String tempPath, VideonaFormat format,
+                                                     MediaTranscoderListener listener, String text,
+                                                     String textPosition, int startTime, int stopTime) throws IOException {
+
+        Image imageText = getImageFromTextAndPosition(text, textPosition);
+
+        MediaTranscoder.getInstance().transcodeTrimAndOverlayImageToVideo(mediaPath,
+                tempPath,format, listener, imageText, startTime, stopTime);
+    }
+
+    @NonNull
+    public Image getImageFromTextAndPosition(String text, String textPosition) {
+        Drawable textDrawable = TextToDrawable.createDrawableWithTextAndPosition(text, textPosition);
+
+        return new Image(textDrawable, Constants.DEFAULT_VIMOJO_WIDTH,Constants.DEFAULT_VIMOJO_HEIGHT);
+    }
+
 
 }
 
---
+++ app/src/test/java/com/videonasocialmedia/vimojo/text/ModifyVideoTextAndPositionUseCaseTest.java
@@ -0,0 +1,8 @@
+package com.videonasocialmedia.vimojo.text;
+/**
+ * Created by alvaro on 8/09/16.
+ */
+public class ModifyVideoTextAndPositionUseCaseTest {
+
+    // TODO:(alvaro.martinez) 8/09/16 ModifyVideoTextAndPositionUseCaseTest
+}
