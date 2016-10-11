056bccc107b4a2a430c57adf2d015053a85071e8
Javier Liarte <jliarte@gmail.com>
2016-10-11 12:30:52 +0200

    Capturing NoSuchElementException bug com.videonasocialmedia.muxer.utils.Utils.createFile from MergeVoiceOverAudiosUseCase.mergeAudio caught in 20161011 debug session w/pablo and sending it to crashlytics

--- app/src/main/java/com/videonasocialmedia/vimojo/sound/domain/MergeVoiceOverAudiosUseCase.java
+++ app/src/main/java/com/videonasocialmedia/vimojo/sound/domain/MergeVoiceOverAudiosUseCase.java
@@ -2,6 +2,7 @@ package com.videonasocialmedia.vimojo.sound.domain;
 
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.googlecode.mp4parser.authoring.Movie;
 import com.videonasocialmedia.muxer.Appender;
 import com.videonasocialmedia.vimojo.model.entities.editor.media.Music;
@@ -14,12 +15,14 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.NoSuchElementException;
 
 /**
  * Created by alvaro on 16/09/16.
  */
 public class MergeVoiceOverAudiosUseCase {
 
+    private static final String TAG = "MergeVoiceOverAudiosUC";
     OnMergeVoiceOverAudiosListener listener;
 
     public MergeVoiceOverAudiosUseCase(OnMergeVoiceOverAudiosListener listener){
@@ -61,7 +64,16 @@ public class MergeVoiceOverAudiosUseCase {
             listener.onMergeVoiceOverAudioSuccess(pathAudioEdited);
         } catch (IOException | NullPointerException e) {
             listener.onMergeVoiceOverAudioError(String.valueOf(e));
+        } catch (NoSuchElementException e) {
+            logExceptionWithMessage(e, "saveFinalVideo: Exception caught in 20161011 debugging session w/ pablo");
+            listener.onMergeVoiceOverAudioError(String.valueOf(e));
         }
+    }
+
+    private void logExceptionWithMessage(NoSuchElementException e, String msg) {
+        Crashlytics.log(msg);
+        Crashlytics.logException(e);
+        Log.d(TAG, msg+" - "+String.valueOf(e));
     }
 
     private Movie appendVideos(ArrayList<String> videoTranscodedPaths, boolean addOriginalAudio) {
