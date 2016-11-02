80aa63c3a7fd5d968d214f9d8babffa14a36117d
ruth <r.toro@videona.com>
2016-11-02 17:37:23 +0100

    Rename and change the colors used in the app Vimojo

--- app/src/main/java/com/videonasocialmedia/vimojo/sources/MusicSource.java
+++ app/src/main/java/com/videonasocialmedia/vimojo/sources/MusicSource.java
@@ -30,15 +30,16 @@ public class MusicSource {
         }
     }
 
+
     protected void populateLocalMusic() {
         localMusic.add(new Music(R.drawable.ic_free_the_cold_wind_george_stephenson_bradford_lawrence_ellis, "Free the cold wind",
-                R.raw.free_the_gold_wind, R.color.folk, "George Stephenson, Bradford Lawrence Ellis", "02:45"));
+                R.raw.free_the_gold_wind, R.color.colorPrimary, "George Stephenson, Bradford Lawrence Ellis", "02:45"));
         localMusic.add(new Music(R.drawable.ic_galloping_a_stuart_roslyn_matt_foundling, "Galloping",
-                R.raw.galloping, R.color.folk, "Stuart Roslyn, Matt Foundling", "02:01"));
+                R.raw.galloping, R.color.colorPrimary, "Stuart Roslyn, Matt Foundling", "02:01"));
         localMusic.add(new Music(R.drawable.ic_sorrow_and_sadness_b_david_john_vanacore_ehren_ebbage, "Sorrow and sadness",
-                R.raw.sorrow_and_sadness_b, R.color.folk, "David Jhon, Vanacore Ehrenebbage", "01:33"));
+                R.raw.sorrow_and_sadness_b, R.color.colorPrimary, "David Jhon, Vanacore Ehrenebbage", "01:33"));
         localMusic.add(new Music(R.drawable.ic_we_beat_as_one_b_harlin_james_paul_lewis1, "We beat as one",
-                R.raw.we_beat_as_one_b, R.color.folk, "Harlin James, Paul Lewis", "03:30"));
+                R.raw.we_beat_as_one_b, R.color.colorPrimary, "Harlin James, Paul Lewis", "03:30"));
     }
 
     public Music getMusicByTitle(String musicTitle) {
