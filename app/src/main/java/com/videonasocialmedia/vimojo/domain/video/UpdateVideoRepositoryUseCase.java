966b2ef7b46ecac1c15f6bc2463d4f7dfcd9fd0a
Javier Liarte <jliarte@gmail.com>
2017-07-24 09:57:46 +0200

    Hotfixed trim transcode job success and error callbacks that didnt update Video error. Fixed UpdateVideoRepositoryUseCase.succesTranscodingVideo
    - Updated TrimPreviewPresenter to call updateVideoRepositoryUseCase.succesTranscodingVideo on transcoding success and updateVideoRepositoryUseCase.errorTranscodingVideo on transcoding error
    - Modified UpdateVideoRepositoryUseCase.succesTranscodingVideo to reset video error calling video.setVideoError

--- app/src/main/java/com/videonasocialmedia/vimojo/domain/video/UpdateVideoRepositoryUseCase.java
+++ app/src/main/java/com/videonasocialmedia/vimojo/domain/video/UpdateVideoRepositoryUseCase.java
@@ -22,6 +22,7 @@ public class UpdateVideoRepositoryUseCase {
   public void succesTranscodingVideo(Video video){
     video.resetNumTriesToExportVideo();
     video.setTranscodingTempFileFinished(true);
+    video.setVideoError(null);
     videoRepository.update(video);
   }
 
