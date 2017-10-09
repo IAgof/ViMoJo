db0ae1781cdeb72d4f4cdee84404c9fbe669551b
ruth <r.toro@videona.com>
2017-10-09 16:47:43 +0200

    -VimojoLicenseProvider : source license added. The license have been taken from dependency to build.gradle.

--- app/src/main/java/com/videonasocialmedia/vimojo/settings/licensesVimojo/source/VimojoLicensesProvider.java
+++ app/src/main/java/com/videonasocialmedia/vimojo/settings/licensesVimojo/source/VimojoLicensesProvider.java
@@ -27,9 +27,31 @@ public class VimojoLicensesProvider {
   }
 
   private void populateLicenseList() {
-    licenseList.add(new LicenseVimojo("Licencias de código abierto", context.getString(R.string.licenseContent)));
-    licenseList.add(new LicenseVimojo("Licencias de diseño", context.getString(R.string.licenseDesign)));
-    licenseList.add(new LicenseVimojo("Licencias de música", context.getString(R.string.licenseMusic)));
+    licenseList.add(new LicenseVimojo("Design", context.getString(R.string.licenseDesign)));
+    licenseList.add(new LicenseVimojo("Music", context.getString(R.string.licenseMusic)));
+    licenseList.add(new LicenseVimojo("Exo Player",context.getString(R.string.licenseExoPlayer)));
+    licenseList.add(new LicenseVimojo("Dagger", context.getString(R.string.licenseDagger)));
+    licenseList.add(new LicenseVimojo("EventBus",context.getString(R.string.licenseEventBus)));
+    licenseList.add(new LicenseVimojo("Glide", context.getString(R.string.licenseGlide)));
+    licenseList.add(new LicenseVimojo("Google GSon", context.getString(R.string.licenseGoogleGSon)));
+    licenseList.add(new LicenseVimojo("Guava", context.getString(R.string.licenseGuava)));
+    licenseList.add(new LicenseVimojo("JCraft JSch ",context.getString(R.string.licenseJcraftJSch)));
+    licenseList.add(new LicenseVimojo("JCraft JZlib ",context.getString(R.string.licenseJcraftJZ)));
+    licenseList.add(new LicenseVimojo("JCodec", context.getString(R.string.licenseJCodec)));
+    licenseList.add(new LicenseVimojo("Jerzy Chalupski. Floating Action Button",context.getString
+        (R.string.licenseFloatingActionButton)));
+    licenseList.add(new LicenseVimojo("Karumi",context.getString(R.string.licenseKarumi)));
+    licenseList.add(new LicenseVimojo("MixPanel, Inc",context.getString(R.string.licenseMixPanel)));
+    licenseList.add(new LicenseVimojo("Range SeekBar ",context.getString(R.string.licenseRangeSeekBar)));
+    licenseList.add(new LicenseVimojo("Realm Android Adapters",context.getString(R.string.licenseRealm)));
+    licenseList.add(new LicenseVimojo("Roughike. BottomBar",context.getString(R.string.licenseRoughikeBottomBar)));
+    licenseList.add(new LicenseVimojo("Square OKhttp",context.getString(R.string.licenseSquare)));
+    licenseList.add(new LicenseVimojo("The Android Open Source Project",context.getString
+        (R.string.licenseAndroidOpenSource)));
+    licenseList.add(new LicenseVimojo("Vertical Seekbar",context.getString(R.string.licenseVerticalSeekbar)));
+    licenseList.add(new LicenseVimojo("Wasabeef",context.getString(R.string.licenseWasabeef)));
+    licenseList.add(new LicenseVimojo("Yankai-Victor. Loading",context.getString(R.string.licenseYankaiVictor)));
+    licenseList.add(new LicenseVimojo("Apache Commons Net",context.getString(R.string.licenseApacheCommonsNet)));
 
   }
 }
