ee3b67dd6cade3da78a4d4a93198a96cf742ef95
Javier Liarte <jliarte@gmail.com>
2017-10-31 18:33:43 +0100

    Modified VideoThumbnailGenerateParamsBitmapResourceDecoder to use a context WeakReference to avoid memory leaks

--- app/src/main/java/com/videonasocialmedia/vimojo/presentation/views/adapter/timeline/helper/VideoThumbnailGenerateParamsBitmapResourceDecoder.java
+++ app/src/main/java/com/videonasocialmedia/vimojo/presentation/views/adapter/timeline/helper/VideoThumbnailGenerateParamsBitmapResourceDecoder.java
@@ -11,28 +11,32 @@ import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool;
 import com.bumptech.glide.load.resource.bitmap.BitmapResource;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 
 /** Handles pooling to reduce/prevent GC lagging from too many {@link Bitmap#createBitmap}s */
 public class VideoThumbnailGenerateParamsBitmapResourceDecoder
         implements ResourceDecoder<VideoThumbnailGenerateParams, Bitmap> {
-  private final Context context;
+  private final WeakReference<Context> contextWeakReference;
 
   public VideoThumbnailGenerateParamsBitmapResourceDecoder(Context context) {
-    this.context = context;
+    this.contextWeakReference = new WeakReference<Context>(context);
   }
 
   @Override
   public Resource<Bitmap> decode(VideoThumbnailGenerateParams source, int width, int height)
           throws IOException {
-    BitmapPool pool = Glide.get(context).getBitmapPool();
-    MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-    retriever.setDataSource(source.video.getMediaPath());
-    long microSecond = source.video.getStartTime() * 1000;
-    Bitmap frameAtTime = retriever.getFrameAtTime(microSecond,
-            MediaMetadataRetriever.OPTION_CLOSEST);
-    retriever.release();
-    Bitmap bitmap = frameAtTime;
-    return BitmapResource.obtain(bitmap, pool);
+    if (contextWeakReference.get() != null) {
+      BitmapPool pool = Glide.get(contextWeakReference.get()).getBitmapPool();
+      MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+      retriever.setDataSource(source.video.getMediaPath());
+      long microSecond = source.video.getStartTime() * 1000;
+      Bitmap frameAtTime = retriever.getFrameAtTime(microSecond,
+              MediaMetadataRetriever.OPTION_CLOSEST);
+      retriever.release();
+      Bitmap bitmap = frameAtTime;
+      return BitmapResource.obtain(bitmap, pool);
+    }
+    return null;
   }
 
   @Override
