51a378d1c4b0c5ec70cd612cf4074c3792211aac
Javier Liarte <jliarte@gmail.com>
2017-11-16 13:25:56 +0100

    Updated licenses with Samsung SDK license

--- app/src/main/java/com/videonasocialmedia/vimojo/settings/licensesVimojo/source/VimojoLicensesProvider.java
+++ app/src/main/java/com/videonasocialmedia/vimojo/settings/licensesVimojo/source/VimojoLicensesProvider.java
@@ -60,5 +60,6 @@ public class VimojoLicensesProvider {
     licenseList.add(new LicenseVimojo("Vertical Seekbar",context.getString(R.string.licenseVerticalSeekbar)));
     licenseList.add(new LicenseVimojo("Apache Commons Net",context.getString(R.string.licenseApacheCommonsNet)));
     licenseList.add(new LicenseVimojo("OmRecorder", context.getString(R.string.licenseOmRecorder)));
+    licenseList.add(new LicenseVimojo("Samsung SDK", context.getString(R.string.licenseSamsungSDK)));
   }
 }
