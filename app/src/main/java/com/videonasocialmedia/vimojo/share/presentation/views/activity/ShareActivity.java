e66ae641f28cc7184b407f47fe4dc40e6464cbed
alvaro <a.martinez@videona.com>
2018-04-27 11:23:42 +0200

    Added feature share show social network, hide/show social networks for every flavor with a feature toggle. - ShareVideoPresenter, obtainNetworkToShare, if feature activated get socialNetworkList else hide fab button. ObtainListOptionToShare, add social network list if feature is activated. - ShareActivity, added field fab share room and implement hideShowMoreSocialNetworks, set visibility gone. - ShareVideoView, added method hideShowMoreSocialNetworks - activity_share.xml Added center in parent to share list card view, improve design when feature is not activated. - build.gradle added feature toggle FEATURE_SHARE_SHOW_SOCIAL_NETWORKS, activated for every flavor except M4N.

--- app/src/main/java/com/videonasocialmedia/vimojo/share/presentation/views/activity/ShareActivity.java
+++ app/src/main/java/com/videonasocialmedia/vimojo/share/presentation/views/activity/ShareActivity.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
@@ -74,6 +75,8 @@ public class ShareActivity extends EditorActivity implements ShareVideoView,
     BottomBar bottomBar;
     @BindView(R.id.fab_edit_room)
     FloatingActionsMenu fabMenu;
+    @Nullable @BindView(R.id.fab_share_room)
+    FloatingActionButton fabShareRoom;
     private OptionsToShareAdapter optionsShareAdapter;
 
   private ProgressDialog exportProgressDialog;
@@ -326,6 +329,11 @@ public class ShareActivity extends EditorActivity implements ShareVideoView,
         exportProgressDialog.dismiss();
       }
     });
+  }
+
+  @Override
+  public void hideShowMoreSocialNetworks() {
+    fabShareRoom.setVisibility(View.GONE);
   }
 
   private void navigateToProjectDetails() {
